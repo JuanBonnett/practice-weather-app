7938dda5bf7996aa0a993183a3b8263a7caf2840
Sebastian Bonnett <juan.xd.net@gmail.com>
2024-03-04 11:03:11 -0500

    code cleanup

--- app.js
+++ app.js
@@ -260,7 +260,7 @@ function locationFieldAutoComplete() {
 async function initMap(lat, lng) {
     //Weird code from Google Documentation to grab the MAP API
     (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-        ({key: "AIzaSyAuUWPAVmaPABhNTcGGKtrPT-4gLdvL-f4", v: "weekly"});
+        ({key: G_KEY, v: "weekly"});
 
     const position = { lat : lat, lng : lng };
     const { Map } = await google.maps.importLibrary('maps');
@@ -291,7 +291,6 @@ function getWeatherFromCoords(lat, lng) {
 
     gMap.map.setCenter({ lat : parseFloat(lat), lng : parseFloat(lng) });
     gMap.marker.position = { lat : parseFloat(lat), lng : parseFloat(lng) };
-    APP_STATE.updateMap(gMap.map, gMap.marker);
     APP_STATE.updateCoords(lat, lng);
     APP_STATE.toggleGeoLocation(false);
     updateCoordsText(lat, lng);
